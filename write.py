68f7b4e03d79715bdf0236a475a52438562b8d22
Chieh <chieh_tsai@solomon-3d.com>
2024-01-04 16:07:52 +0800

    update

--- write.py
+++ write.py
@@ -22,20 +22,22 @@ if not client.connect():  # .connect() returns True if connection established
     print("[Error] Fail to connect to modbus slave %s:%d." % (SERVER_IP, SERVER_PORT))
     exit()
 
-res = client.read_holding_registers(address=99, count=10, unit=1)
+res = client.read_holding_registers(address=ADDRESS_START, count=10, unit=DEVICE_ID)
 results = res.registers  # shows the result, type: list
 print(results)
 
 # ---------------------------
 ## write an random value
-print("[INFO] Now writing a random boolean value to address= {}.".format(ADDRESS_START))
-rand_value = random.randint(10, 19)
+print("[INFO] Now writing a random boolean value to address = {}.".format(ADDRESS_START))
+rand_value = random.randint(10, 10000)
 print(rand_value)
 write_registers_response = client.write_register(
     address=ADDRESS_START, value=rand_value, unit=DEVICE_ID)
 write_registers_response.isError()
 
-res = client.read_holding_registers(address=99, count=10, unit=1)
+# ---------------------------
+# read the values
+res = client.read_holding_registers(address=ADDRESS_START, count=10, unit=DEVICE_ID)
 results = res.registers  # shows the result, type: list
 print(results)
 
