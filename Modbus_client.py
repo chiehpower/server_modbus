ea750556431130735f7acee91f0f84ede8a560fa
Chieh <chieh_tsai@solomon-3d.com>
2024-01-04 11:48:05 +0800

    update

--- Modbus_client.py
+++ Modbus_client.py
@@ -1,6 +1,21 @@
 import socket
 import time
 
+
+def generate_modbus_rtu_message(address, function_code, register_address, data):
+    # Convert values to hex strings
+    address_hex = format(address, '02x')
+    function_code_hex = format(function_code, '02x')
+    register_address_hex = format(register_address, '04x')
+    data_hex = format(data, '04x')
+
+    # Concatenate the hex strings to form the Modbus RTU message
+    modbus_rtu_message = address_hex + \
+        function_code_hex + register_address_hex + data_hex
+
+    return modbus_rtu_message
+
+
 def main():
     host = '0.0.0.0'  # Server IP address
     port = 9898       # Server port
@@ -14,7 +29,16 @@ def main():
         print("Connected to server")
 
         # Send data to the server
-        hex_message = '00010000000600050000FF00' # Hex representation
+        # 使用範例
+        address = 1         # Modbus 地址
+        function_code = 6   # 功能碼：寫單個保持寄存器
+        register_address = 100  # 寄存器地址
+        data = 2            # 要寫入的數據
+
+        hex_message = generate_modbus_rtu_message(
+            address, function_code, register_address, data)
+        print("Hex Message:", hex_message)
+        # hex_message = '00010000000600050000FF00' # Hex representation
         print("len = ", len(hex_message))
         print(bytes.fromhex(hex_message))
         client_socket.send(bytes.fromhex(hex_message))
